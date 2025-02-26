00f5c13dd67087019bf6ebe38e5964541cd8a679
Mukit <abdullahalmukit287@gmail.com>
2025-02-26 21:06:54 +0600

    done

---
+++ Week 02 - Cycle Detection and Dijkstra/Maze.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char grid[105][105];
+bool vis[105][105];
+int level[105][105];
+pair<int, int> parent[105][105];
+vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+int n, m;
+
+bool valid(int i, int j) {
+    return i >= 0 && i < n && j >= 0 && j < m && (grid[i][j] == '.' || grid[i][j] == 'D');
+}
+
+void bfs(int si, int sj) {
+    queue<pair<int, int>> q;
+    q.push({si, sj});
+    vis[si][sj] = true;
+    level[si][sj] = 0;
+
+    while (!q.empty()) {
+        pair<int, int> par = q.front();
+        q.pop();
+        int par_i = par.first;
+        int par_j = par.second;
+
+        for (int i = 0; i < 4; i++) {
+            int ci = par_i + d[i].first;
+            int cj = par_j + d[i].second;
+
+            if (valid(ci, cj) && !vis[ci][cj]) {
+                q.push({ci, cj});
+                vis[ci][cj] = true;
+                level[ci][cj] = level[par_i][par_j] + 1;
+                parent[ci][cj] = {par_i, par_j};
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+
+    int si = -1, sj = -1; 
+    int di = -1, dj = -1; 
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+            if (grid[i][j] == 'R') {
+                si = i;
+                sj = j;
+            } else if (grid[i][j] == 'D') {
+                di = i;
+                dj = j;
+            }
+        }
+    }
+
+    if (si == -1 || sj == -1 || di == -1 || dj == -1) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cout << grid[i][j];
+            }
+            cout << endl;
+        }
+        return 0;
+    }
+
+    if (si == di && sj == dj) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cout << grid[i][j];
+            }
+            cout << endl;
+        }
+        return 0;
+    }
+
+    memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
+    memset(parent, -1, sizeof(parent));
+    bfs(si, sj);
+
+    if (level[di][dj] != -1) {
+        int x = di, y = dj;
+        while (x != si || y != sj) {
+            if (grid[x][y] == '.') {
+                grid[x][y] = 'X';
+            }
+            pair<int, int> par = parent[x][y];
+            x = par.first;
+            y = par.second;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << grid[i][j];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
